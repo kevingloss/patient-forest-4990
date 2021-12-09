5f0eec226c52b2e428311cc14ac6cb31dc33dbb5
Kevin Gloss <kgloss89@gmail.com>
2021-12-09 11:22:27 -0700

    Test the names of actors are shown

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -11,7 +11,10 @@ RSpec.describe 'movies show page' do
       @allen = @raiders.actors.create!(name: 'Karen Allen', age: 70)
       @freeman = @raiders.actors.create!(name: 'Paul Freeman', age: 78)
 
-
+      @pixar = Studio.create!(name: 'Pixar Studios', location: 'San Francisco')
+      @toy = @pixar.movies.create!(title: 'Toy Story', creation_year: 2004, genre: 'Family')
+      @monster = @pixar.movies.create!(title: 'Monster Inc.', creation_year: 2001, genre: 'Comedy')
+      @buzz = @toy.actors.create!(name: 'Buzz Lightyear', age: 27)
     end
 
     it 'has the movie title, creation year, and genre' do
@@ -20,19 +23,23 @@ RSpec.describe 'movies show page' do
       expect(page).to have_content(@raiders.title)
       expect(page).to have_content(@raiders.creation_year)
       expect(page).to have_content(@raiders.genre)
+      expect(page).to_not have_content(@toy.title)
     end
 
-    it 'has a list of its actors from youngest to oldest' do
+    it 'has a list of its actors' do
       visit "/movies/#{@raiders.id}"
 
       expect(page).to have_content(@ford.name)
       expect(page).to have_content(@allen.name)
       expect(page).to have_content(@freeman.name)
+      expect(page).to_not have_content(@buzz.name)
+    end
+    it 'sorts the actors youngest to oldest' do
+      visit "/movies/#{@raiders.id}"
 
       expect(@allen.name).to appear_before(@ford.name)
       expect(@ford.name).to appear_before(@freeman.name)
     end
-
     it 'has the average age of all the actors in the movie'
   end
 end
