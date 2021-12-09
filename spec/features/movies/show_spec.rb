02a1b0678e84e31d96faca1e16bea632886d2f9f
Kevin Gloss <kgloss89@gmail.com>
2021-12-09 09:58:30 -0700

    Test movie show page for movie attributes

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'movies show page' do
+  describe 'when visiting' do
+    before :each do
+      @universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+      @raiders = @universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+      @shrek = @universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+      # create actors
+
+
+
+    end
+
+    it 'has the movie title, creation year, and genre' do
+      visit '/movies/1'
+
+      expect(page).to have_content(@raiders.title)
+      expect(page).to have_content(@raiders.creation_year)
+      expect(page).to have_content(@raiders.genre)
+    end
+
+    it 'has a list of its actors from youngest to oldest'
+    it 'has the average age of all the actors in the movie'
+  end
+end
