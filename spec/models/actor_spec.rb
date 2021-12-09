2c3b573a839a14be1fc74ba6db79fd06c16eaa57
Kevin Gloss <kgloss89@gmail.com>
2021-12-09 11:28:52 -0700

    Test for by_age_asc

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Actor, type: :model do
     it { should have_many :actor_movies}
     it { should have_many(:movies).through(:actor_movies) }
   end
+
+  it 'sorts actors by age asc' do
+    ford = Actor.create!(name: 'Harrison Ford', age: 78)
+    allen = Actor.create!(name: 'Karen Allen', age: 70)
+    freeman = Actor.create!(name: 'Paul Freeman', age: 78)
+
+    expect(Actor.by_age_asc).to eq([allen, ford, freeman])
+  end
+
 end
